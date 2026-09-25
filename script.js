e586dbdd939f4f9734dc60c543c1fc30fba1f929
Barbara Soares <barbarasoares1995@hotmail.com>
2026-09-25 08:28:45 -0300

    Usar horário atual do dispositivo

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function carregar() {
     const msg = window.document.getElementById ('msg')
     const img = window.document.getElementById('imagem')
     const data = new Date()
-    // const hora = data.getHours()
-    const hora = 20
+    const hora = data.getHours()
+
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
